6814fd4d83180d154db50b5cea1cf2a2d12f3c04
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-04-24 19:10:28 +0200

    day 9

--- basket/lib/main.dart
+++ basket/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,7 +7,19 @@ void main() {
 }
 
 class BasketBall extends StatelessWidget {
-  const BasketBall({super.key});
+  int TeamAPoints = 0;
+  int TeamBPoints = 0;
+
+  BasketBall({super.key});
+
+  void AddPointsA(int points) {
+    print('Adding points to Team A');
+    TeamAPoints += points;
+  }
+
+  void AddPointsB(int points) {
+    TeamBPoints += points;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,21 +34,258 @@ class BasketBall extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text(
-              'Team A',
-              style: TextStyle(
-                fontSize: 30,
-                color: const Color.fromARGB(255, 2, 3, 4),
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              '0',
-              style: TextStyle(
-                fontSize: 145,
-                color: const Color.fromARGB(255, 0, 0, 0),
-                fontWeight: FontWeight.bold,
-              ),
+            Row(
+              children: [
+                Spacer(flex: 1),
+                Container(
+                  height: 550,
+                  child: Column(
+                    children: [
+                      Text(
+                        'Team A',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: const Color.fromARGB(255, 2, 3, 4),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '$TeamAPoints',
+                        style: TextStyle(
+                          fontSize: 145,
+                          color: const Color.fromARGB(255, 0, 0, 0),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: const Size(160, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          backgroundColor: const Color.fromARGB(
+                            255,
+                            225,
+                            163,
+                            30,
+                          ), // background
+                          foregroundColor: const Color.fromARGB(
+                            255,
+                            8,
+                            8,
+                            5,
+                          ), // foreground
+                        ),
+                        onPressed: () => AddPointsA,
+                        child: Text(
+                          'Add 1 points',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: const Color.fromARGB(255, 0, 0, 0),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Spacer(flex: 1),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: const Size(160, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          backgroundColor: const Color.fromARGB(
+                            255,
+                            225,
+                            163,
+                            30,
+                          ), // background
+                          foregroundColor: const Color.fromARGB(
+                            255,
+                            8,
+                            8,
+                            5,
+                          ), // foreground
+                        ),
+                        onPressed: () {
+                          // Add
+                        },
+                        child: Text(
+                          'Add 2 points',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: const Color.fromARGB(255, 0, 0, 0),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Spacer(flex: 1),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: const Size(160, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          backgroundColor: const Color.fromARGB(
+                            255,
+                            225,
+                            163,
+                            30,
+                          ), // background
+                          foregroundColor: const Color.fromARGB(
+                            255,
+                            8,
+                            8,
+                            5,
+                          ), // foreground
+                        ),
+                        onPressed: () {
+                          // Add
+                        },
+                        child: Text(
+                          'Add 3 points',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: const Color.fromARGB(255, 0, 0, 0),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Spacer(flex: 13),
+                    ],
+                  ),
+                ),
+                Spacer(flex: 1),
+                // Vertical Divider
+                Container(
+                  height: 400,
+                  child: VerticalDivider(
+                    color: Color.fromARGB(255, 222, 159, 66),
+                    thickness: 1,
+                    width: 20,
+                  ),
+                ),
+                Spacer(flex: 1),
+                Container(
+                  height: 550,
+                  child: Column(
+                    children: [
+                      Text(
+                        'Team B',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: const Color.fromARGB(255, 2, 3, 4),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '$TeamBPoints',
+                        style: TextStyle(
+                          fontSize: 145,
+                          color: const Color.fromARGB(255, 0, 0, 0),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: const Size(160, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          backgroundColor: const Color.fromARGB(
+                            255,
+                            225,
+                            163,
+                            30,
+                          ), // background
+                          foregroundColor: const Color.fromARGB(
+                            255,
+                            8,
+                            8,
+                            5,
+                          ), // foreground
+                        ),
+                        onPressed: () {
+                          // Add
+                        },
+                        child: Text(
+                          'Add 1 points',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: const Color.fromARGB(255, 0, 0, 0),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Spacer(flex: 1),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: const Size(160, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          backgroundColor: const Color.fromARGB(
+                            255,
+                            225,
+                            163,
+                            30,
+                          ), // background
+                          foregroundColor: const Color.fromARGB(
+                            255,
+                            8,
+                            8,
+                            5,
+                          ), // foreground
+                        ),
+                        onPressed: () {
+                          // Add
+                        },
+                        child: Text(
+                          'Add 2 points',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: const Color.fromARGB(255, 0, 0, 0),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Spacer(flex: 1),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: const Size(160, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          backgroundColor: const Color.fromARGB(
+                            255,
+                            225,
+                            163,
+                            30,
+                          ), // background
+                          foregroundColor: const Color.fromARGB(
+                            255,
+                            8,
+                            8,
+                            5,
+                          ), // foreground
+                        ),
+                        onPressed: () {
+                          // Add
+                        },
+                        child: Text(
+                          'Add 3 points',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: const Color.fromARGB(255, 0, 0, 0),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Spacer(flex: 13),
+                    ],
+                  ),
+                ),
+                Spacer(flex: 1),
+              ],
             ),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
@@ -55,12 +306,11 @@ class BasketBall extends StatelessWidget {
                   5,
                 ), // foreground
               ),
-
               onPressed: () {
                 // Add
               },
               child: Text(
-                'Add 1 points',
+                'Reset',
                 style: TextStyle(
                   fontSize: 18,
                   color: const Color.fromARGB(255, 0, 0, 0),
@@ -68,74 +318,32 @@ class BasketBall extends StatelessWidget {
                 ),
               ),
             ),
-            Spacer(flex: 1),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                fixedSize: const Size(160, 50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                backgroundColor: const Color.fromARGB(
-                  255,
-                  225,
-                  163,
-                  30,
-                ), // background
-                foregroundColor: const Color.fromARGB(
-                  255,
-                  8,
-                  8,
-                  5,
-                ), // foreground
-              ),
-
-              onPressed: () {
-                // Add
-              },
-              child: Text(
-                'Add 2 points',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: const Color.fromARGB(255, 0, 0, 0),
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Spacer(flex: 1),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                fixedSize: const Size(160, 50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                backgroundColor: const Color.fromARGB(
-                  255,
-                  225,
-                  163,
-                  30,
-                ), // background
-                foregroundColor: const Color.fromARGB(
-                  255,
-                  8,
-                  8,
-                  5,
-                ), // foreground
-              ),
-
-              onPressed: () {
-                // Add
-              },
-              child: Text(
-                'Add 3 points',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: const Color.fromARGB(255, 0, 0, 0),
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Spacer(flex: 13),
           ],
+        ),
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: const Color.fromARGB(255, 255, 170, 0),
+          child: const Icon(Icons.sports_basketball),
+          onPressed: () {
+            // Add
+          },
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        bottomNavigationBar: BottomAppBar(
+          color: const Color.fromARGB(255, 255, 170, 0),
+          child: Container(
+            height: 50,
+            child: const Center(
+              child: Text(
+                'Basketball Scoreboard',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
